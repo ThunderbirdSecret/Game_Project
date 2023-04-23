9e2f3d564f73898a917b64ffc592de3a7992503e
Katarina Shalagina <j.milk@yandex.ru>
2023-04-23 23:29:17 +0300

    fix elements

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import './App.scss'
-import { Navigate, Route, BrowserRouter as Router, Routes, useNavigate} from "react-router-dom"
+import { Navigate, Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import Header from './components/header/Header'
 import Main from './pages/main/main'
 import Game from './pages/game/game'
@@ -12,11 +12,7 @@ import Register from './pages/register/register'
 import Auth from './pages/auth/auth'
 
 function App() {
-
-  const navigate = useNavigate()
-  const goBack = () => navigate(-1)
-  const goForward = () => navigate(1)
-
+//TODO: приватные роуты будут готовы после того, как будет готова авторизация
   useEffect(() => {
     const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`
@@ -32,7 +28,6 @@ function App() {
         <div>
           <Header />
           <Routes>
-          <button onClick={goBack}>back</button>
             <Route path="/" element={<Main/>} />
             <Route path="/game" element={<Game/>} />
             <Route path="/forum" element={<Forum/>} />
