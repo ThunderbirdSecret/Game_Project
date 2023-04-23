cf11e0e96fd8e78d175d3b32bc9d3223b49a3ac5
Katarina Shalagina <j.milk@yandex.ru>
2023-04-24 00:32:15 +0300

    fix for lint

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+// export default {
+//   preset: 'ts-jest',
+//   testEnvironment: 'jsdom',
   
-  moduleNameMapper: {
-    '^.*\\.scss$': '<rootDir>/test/unit/stubs/style.js',
-    '^@/(.*)$': '<rootDir>/src/$1',
-    "/^.*\.scss$/": "/home/runner/work/Game_Project/Game_Project/packages/client/test/unit/stubs/style.js"
-  },
-  "resolver": undefined,
-  testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
-  globals: {
-    __SERVER_PORT__: process.env.SERVER_PORT,
-  },
-}
+//   moduleNameMapper: {
+//     '^.*\\.scss$': '<rootDir>/test/unit/stubs/style.js',
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//     "/^.*\.scss$/": "/home/runner/work/Game_Project/Game_Project/packages/client/test/unit/stubs/style.js"
+//   },
+//   "resolver": undefined,
+//   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+//   globals: {
+//     __SERVER_PORT__: process.env.SERVER_PORT,
+//   },
+// }
