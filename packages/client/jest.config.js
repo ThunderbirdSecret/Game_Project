668f4b19b37acdcf614edfa7c0d586428f455f91
Katarina Shalagina <j.milk@yandex.ru>
2023-04-24 00:07:31 +0300

    fix test file for lint

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -4,6 +4,10 @@ dotenv.config()
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^.*\\.scss$': '<rootDir>/test/unit/stubs/style.js',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
