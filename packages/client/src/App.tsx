6fbcbabb00cd3978b03d9650bc9207d0a291fb17
Marina Mokrousova <rar-mar-rar@yandex.ru>
2023-04-28 19:32:47 +0300

    добавляю css переменные

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,10 @@
 import { useEffect } from 'react'
-import { Navigate, Route, BrowserRouter as Router, Routes} from "react-router-dom"
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from 'react-router-dom'
 import Header from './components/header/Header'
 import Main from './pages/main/main'
 import Game from './pages/game/game'
@@ -12,35 +17,40 @@ import Auth from './pages/auth/auth'
 import { ROUTES } from './routes'
 
 function App() {
-//TODO: приватные роуты будут готовы после того, как будет готова авторизация
+  //TODO: приватные роуты будут готовы после того, как будет готова авторизация
   useEffect(() => {
-    const fetchServerData = async () => {
+    /*const fetchServerData = async () => {
       const url = `http://localhost:${__SERVER_PORT__}`
       const response = await fetch(url)
       const data = await response.json()
       console.log(data)
     }
 
-    fetchServerData()
+    fetchServerData()*/
   }, [])
-  return <div className="app">
+  return (
+    <div className="app">
       <Router>
         <div>
           <Header />
           <Routes>
-            <Route path={ROUTES.MAIN} element={<Main/>} />
-            <Route path={ROUTES.GAME} element={<Game/>} />
-            <Route path={ROUTES.FORUM} element={<Forum/>} />
-            <Route path={ROUTES.LEADER_BOARD} element={<LeaderBord/>} />
-            <Route path={ROUTES.PROFILE} element={<Profile/>} />
-            <Route path={ROUTES.AUTH} element={<Auth/>} />
-            <Route path={ROUTES.REGISTER} element={<Register/>} />
-            <Route path={ROUTES.ERROR_404} element={<Error404/>} />
-            <Route path="*" element={<Navigate to={ROUTES.ERROR_404} replace />} />
+            <Route path={ROUTES.MAIN} element={<Main />} />
+            <Route path={ROUTES.GAME} element={<Game />} />
+            <Route path={ROUTES.FORUM} element={<Forum />} />
+            <Route path={ROUTES.LEADER_BOARD} element={<LeaderBord />} />
+            <Route path={ROUTES.PROFILE} element={<Profile />} />
+            <Route path={ROUTES.AUTH} element={<Auth />} />
+            <Route path={ROUTES.REGISTER} element={<Register />} />
+            <Route path={ROUTES.ERROR_404} element={<Error404 />} />
+            <Route
+              path="*"
+              element={<Navigate to={ROUTES.ERROR_404} replace />}
+            />
           </Routes>
         </div>
       </Router>
     </div>
+  )
 }
 
 export default App
--- packages/client/src/pages/leader-board/leader-board.tsx
+++ packages/client/src/pages/leader-board/leader-board.tsx
@@ -1,8 +1,11 @@
+import "./index.scss"
 
 export default function LeaderBord() {
     return (
         <div>
             <h1>LeaderBord</h1>
+            <div className='test-button-gradient'>test button gradient</div>
+            <div className='test-title-gradient'>Worms Game</div>
         </div>
     )
 }
