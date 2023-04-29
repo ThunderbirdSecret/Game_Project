0871b62fc68cdac23fc003fab0616655a3068d36
Marina Mokrousova <rar-mar-rar@yandex.ru>
2023-04-29 17:54:50 +0300

    test vercel

--- packages/client/src/pages/leader-board/leader-board.tsx
+++ packages/client/src/pages/leader-board/leader-board.tsx
@@ -4,7 +4,7 @@ export default function LeaderBord() {
     return (
         <div>
             <h1>LeaderBord</h1>
-            <div className='test-button-gradient'>test button gradient</div>
+            <div className='test-button-gradient'>test button gradient sss</div>
             <div className='test-title-gradient'>Worms Game</div>
         </div>
     )
