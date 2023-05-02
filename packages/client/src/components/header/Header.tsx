d506072ea5efcf4af074082263e7004725b4747c
Katarina Shalagina <64640181+ThunderbirdSecret@users.noreply.github.com>
2023-05-02 21:37:27 +0300

    change header

--- packages/client/src/components/header/Header.tsx
+++ packages/client/src/components/header/Header.tsx
@@ -1,21 +1,41 @@
-import { Link } from "react-router-dom";
-import { items } from "./header-items";
-import "./index.scss"
+import { Link } from 'react-router-dom';
+import { items } from './header-items';
+import './index.scss'
 
 
 
 export default function Header() {
     return (
-        <nav className="header">
-            <ul>
-                {items.map((item, i) => (<li key={i}>
-                    <Link className="link" to={item.link}>{item.title}</Link>
-                </li>))}
-                <li>
-                    <Link className="link" to="/auth">Login /</Link>
-                    <Link className="link" to="/register">Register</Link>
-                </li>
-            </ul>
+        <nav className='header'>
+            <div className='container'>
+                <div className='logo'>
+                    <Link to='/' className='logoContent'>
+                        <img alt="logo" src="/link-main.svg"/>
+                        <p>Burning Worms</p>
+                    </Link>
+                </div>
+                <div className='links'>
+                    <ul>
+                        {items.map((item, i) => (<li key={i}>
+                            <Link className='link' to={item.link}>{item.title}</Link>
+                        </li>))}
+                    </ul>
+                </div>
+                <div className='profile'>
+                    <ul>
+                        <li className='noAuth'>
+                            <Link className='' to='/auth'>Login</Link>
+                            <span>/</span>
+                            <Link className='' to='/register'>Register</Link>
+                        </li>
+                        <li className='yesAuth'>
+                            <button className='btnDropdown'>
+                                <img alt='avatar' src="" />
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
