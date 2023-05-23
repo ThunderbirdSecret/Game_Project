a82f49c88d3fb2e6e6d787ca39bc805f83e11557
Katarina Shalagina <j.milk@yandex.ru>
2023-05-23 22:10:16 +0300

    fix files

--- packages/client/src/canvas/canvas.tsx
+++ packages/client/src/canvas/canvas.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import useCanvas from './useCanvas'
 import style from './index.module.scss'
 import { Fullscreen } from './fullscreen'
@@ -6,11 +5,7 @@ import { Fullscreen } from './fullscreen'
 export const Canvas = (props: any) => {
   const { draw, ...rest } = props
   const canvasRef = useCanvas(draw) // создание и игры и ее постоянное обновление для отображения
-  const [components, setComponents] = useState<any>(canvasRef)
   const label = 'toggle on'
-  useEffect(() => {
-    setComponents(canvasRef.current)
-  }, [])
 
   return (
     <div className={style.screen}>
@@ -20,7 +15,7 @@ export const Canvas = (props: any) => {
         id="canvas"
         {...rest}
       />
-      <Fullscreen toggler={label} ref={components} />
+      <Fullscreen ref={canvasRef} toggler={label} />
     </div>
   )
 }
--- packages/client/src/canvas/fullscreen.tsx
+++ packages/client/src/canvas/fullscreen.tsx
@@ -2,20 +2,22 @@ import TransparentButton from '@/components/ui/transparentButton/TransparentButt
 import { forwardRef } from 'react'
 import style from './index.module.scss'
 
-/* eslint prefer-arrow-callback: ["error", { "allowNamedFunctions": true }] */
-export const Fullscreen = forwardRef(function Fullscreen(props: any, ref: any) {
-  const handleFullScreen = () => {
-    const canvas = ref
-    if (canvas && canvas.requestFullscreen) {
-      canvas.requestFullscreen()
+export const Fullscreen = forwardRef<HTMLCanvasElement, { toggler: string }>(
+  (props, ref) => {
+    const handleFullScreen = () => {
+      if (ref === null || typeof ref !== 'object') return
+      const canvas = ref.current
+      if (canvas && canvas.requestFullscreen) {
+        canvas.requestFullscreen()
+      }
     }
-  }
 
-  return (
-    <TransparentButton
-      className={style.fullscreenBtn}
-      onClick={handleFullScreen}>
-      {props.toggler}
-    </TransparentButton>
-  )
-})
+    return (
+      <TransparentButton
+        className={style.fullscreenBtn}
+        onClick={handleFullScreen}>
+        {props.toggler}
+      </TransparentButton>
+    )
+  }
+)
