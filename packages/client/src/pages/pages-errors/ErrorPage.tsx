5dc4fa50f272e42a1415442cd1438cc945d7fe13
Zwitter911 <sigurd.krupotkin@yandex.ru>
2023-06-02 20:01:10 +0300

    Changed title

--- packages/client/src/pages/pages-errors/ErrorPage.tsx
+++ packages/client/src/pages/pages-errors/ErrorPage.tsx
@@ -10,7 +10,7 @@ interface ErrorPageProps {
 export default function ErrorPage({ code, text }: ErrorPageProps) {
   return (
     <div className={style.error_page}>
-      <p className={style.error_code}>Error {code}</p>
+      <h1 className={style.error_code}>Error {code}</h1>
       <p className={style.error_text}>{text}</p>
       <Link to={ROUTES.MAIN}>Back on main page ➤</Link>
     </div>
