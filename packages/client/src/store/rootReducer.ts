cbd25fba4701ee840b40a09a6ee0418c962f050d
Николай Димитриев <55017954+NikolayDimitriev@users.noreply.github.com>
2023-07-04 15:50:17 +0300

    YAN 78: Переключение тем (клиентская часть) (#32)
    * pull yan-37
    
    * pull yan-36
    
    * removed unnecessary @use variables, change arrow down svg name
    
    * added hook useTheme, added btn to change theme, adapted styles
    
    ---------
    
    Co-authored-by: Nikolay Dimitriev <dimitrievk56@gmail.com>

--- packages/client/src/store/rootReducer.ts
+++ packages/client/src/store/rootReducer.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from '@reduxjs/toolkit'
 
-// eslint-disable-next-line import/no-cycle
 import { userReducer } from './user'
 import { leaderboardReducer } from './leaderboard'
 import { servicesReducer } from './service'
